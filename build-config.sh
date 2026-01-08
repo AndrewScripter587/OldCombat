b4c1e0ac8646c8b3f41cd6ca9fff774c0bc90d60
AndrewScripter587 <145153158+AndrewScripter587@users.noreply.github.com>
2026-01-08 10:35:17 -0500

    Create build-config.sh

---
+++ build-config.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ "$#" -le 1 ]; then
+    echo "Usage: sh build-config.sh build-mode version-scope" >&2
+    exit 1
+fi
+# echo "<%%\n  $1_21_4compile = false\n  lite_compile = true\n  beta_compile = false\n   ultralite = false\n%%>" > ./src/build.mcb
+echo "<%%" > ./src/build.mcb
+
+if [ "$2" == "legacy" ]; then
+  echo "  \$1_21_4compile = true" >> ./src/build.mcb
+else
+  echo "  \$1_21_4compile = false" >> ./src/build.mcb
+fi
+
+if [ "$1" == "full" ]; then
+  echo "  lite_compile = false" >> ./src/build.mcb
+  echo "  ultralite = false" >> ./src/build.mcb
+elif [ "$1" == "lite" ]; then
+  echo "  lite_compile = true" >> ./src/build.mcb
+  echo "  ultralite = false" >> ./src/build.mcb
+elif [ "$1" == "lite" ]; then
+  echo "  lite_compile = false" >> ./src/build.mcb
+  echo "  ultralite = true" >> ./src/build.mcb
+fi
+echo "  beta_compile = false" >> ./src/build.mcb
