80372fffe3abf56084da8da5cae84f306766f7f8
AndrewScripter587 <145153158+AndrewScripter587@users.noreply.github.com>
2026-01-08 10:39:46 -0500

    make some build configuration stuff

--- build-config.sh
+++ build-config.sh
@@ -7,20 +7,21 @@ fi
 # echo "<%%\n  $1_21_4compile = false\n  lite_compile = true\n  beta_compile = false\n   ultralite = false\n%%>" > ./src/build.mcb
 echo "<%%" > ./src/build.mcb
 
-if [ "$2" == "legacy" ]; then
+if [ "$2" = "legacy" ]; then
   echo "  \$1_21_4compile = true" >> ./src/build.mcb
 else
   echo "  \$1_21_4compile = false" >> ./src/build.mcb
 fi
 
-if [ "$1" == "full" ]; then
+if [ "$1" = "full" ]; then
   echo "  lite_compile = false" >> ./src/build.mcb
   echo "  ultralite = false" >> ./src/build.mcb
-elif [ "$1" == "lite" ]; then
+elif [ "$1" = "lite" ]; then
   echo "  lite_compile = true" >> ./src/build.mcb
   echo "  ultralite = false" >> ./src/build.mcb
-elif [ "$1" == "lite" ]; then
+elif [ "$1" = "ultralite" ]; then
   echo "  lite_compile = false" >> ./src/build.mcb
   echo "  ultralite = true" >> ./src/build.mcb
 fi
 echo "  beta_compile = false" >> ./src/build.mcb
+echo "%%>" >> ./src/build.mcb
